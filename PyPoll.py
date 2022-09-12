0a982d10e815659cbc8e7fa7eedb66dd239b6ed7
Nicholas Altizer <altizernicholas@gmail.com>
2022-09-12 10:57:57 -0400

    Adding the election aduit code.

--- PyPoll.py
+++ PyPoll.py
@@ -5,6 +5,23 @@ import os
 # Assign a variable for the file to load and the path.
 file_to_load = os.path.join("resources", "election_results.csv")
 
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Initialize a total vote counter.
+total_votes = 0
+
+# Candidate Options and Candidate Votes
+candidate_options = []
+
+# Declare the empty dictionary
+candidate_votes = {}
+
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open the election results and read the file.
     #election_data = open(file_to_load, 'r')
 with open(file_to_load) as election_data:
@@ -16,19 +33,68 @@ with open(file_to_load) as election_data:
 ## Read the file object with the reader function
     file_reader = csv.reader(election_data)
 
-### Print each row in the CSV file
-## for row in file_reader:
-##    print(row)
-
 # Read and print the header row.
-headers = next(file_reader)
-print(headers)
+    headers = next(file_reader)
 
-#close the file.
-election_data.close()
+    ### Print each row in the CSV file
+    for row in file_reader:
+        # Add to the total vote count.
+        total_votes += 1
 
-# Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+        # Print the candidate name from each row
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            #Add the candidate name to the candidate list.
+            candidate_options.append(candidate_name)
+
+            # Begin tracking that candidate's vote count
+            candidate_votes[candidate_name] = 0
+
+        # Add a vote to that candidate's count
+        candidate_votes[candidate_name] += 1
+
+# Determine the percentage of votes for each candidate by looping through the counts.
+# 1. Iterate through the candidate list.
+for candidate_name in candidate_votes:
+    # 2. retrive vote count of candidate.
+    votes = candidate_votes[candidate_name]
+    # 3. Calculate the percentage of votes.
+    vote_percentage = float(votes) / float(total_votes) * 100
+    ### 4. Print the candidate name and percentage of votes.
+    ##print(f"{candidate_name}: recieved {vote_percentage:.1f}% of the vote.")
+
+    # To do: print out each candidate's name, vote count, and percentage of votes to the terminal
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+    # Determine winning vote count and candidate.
+    # Determine if the votes is greater than the winning count.
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        # If true then set winning_count = votes and winning_percent = voter_percentage
+        winning_count = votes
+        winning_percentage = vote_percentage
+        # Set the winning_candidate equal to the candidate's name
+        winning_candidate = candidate_name
+
+# To do: print out the winning candidate, vote count and percentage to terminal
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+
+print(winning_candidate_summary)
+
+### Print the candidate vote dictionary
+## print(candidate_votes)
+
+### Print the total votes.
+##print(total_votes)
+
+### close the file.
+## election_data.close()
 
 ### Using the open() function with the "w" mode we will wrate data to the file.
 ## outfile = open(file_to_save, "w")
@@ -58,3 +124,6 @@ with open(file_to_save, "w") as txt_file:
 # 3. The percentage of votes each candidate won
 # 4. The total number of votes each candidate won
 # 5. the winner of the election based on popular vote.
+
+### close the file.
+election_data.close()
